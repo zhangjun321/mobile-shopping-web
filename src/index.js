71d969325d45167c0c84f0ae66099ae3a9d2c18e
Jack Pudge <zhangjun_kurt@126.com>
2021-04-08 22:30:08 +0300

    2021/04/08 update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import app from './components/App.vue';
 import {Header} from 'mint-ui'
 Vue.component(Header.name,Header)
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
 
 
